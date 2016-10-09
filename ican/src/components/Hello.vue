3cf45b091bf51204f9519689b20cdfaba2e113b1
DESKTOP-Q0Q0TNP\mine <lizhiliang@xinshi.net>
2016-10-09 13:17:53 +0800

    no message

---
+++ ican/src/components/Hello.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <div v-if="show" transition="expand">hellosdd</div>
+    <h1>{{returnObj}}</h1>
+    <input type="text" v-model="msg">
+    <button v-on:click='chan'>transition</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hsdfsd!',
+      show: false,
+      apiUrl: 'http://211.149.193.19:8080/api/customers',
+      returnObj: []
+    }
+  },
+  ready () {
+    this.chan()
+  },
+  methods: {
+    chan () {
+      this.$http.get(this.apiUrl).then((response) => {
+        console.log(response.status)
+        console.log(response.statusText)
+        console.log(response.body)
+        console.log(response.json())
+      }, (response) => {
+        console.log(response.data)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+.expand-transition {
+  transition: all .3s ease;
+  height: 30px;
+  padding: 10px;
+  background-color: #eee;
+  overflow: hidden;
+}
+.expand-enter, .expand-leave  {
+  height: 0;
+  padding: 0 10px;
+  opacity: 0;
+}
+</style>
---
+++ ican/src/components/SB.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="SB">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 4,
+      follo2: false,
+      follo3: false,
+      pdd: {
+        color: 'black'
+      },
+      items: [
+        {name: 'lzl'},
+        {name: 'xxx'},
+        {name: 'zzz'}
+      ]
+    }
+  },
+  computed: {
+    b: {
+      get () {
+        return this.msg + 1
+      }
+    }
+  },
+  methods: {
+    changej () {
+      this.items.unshift()
+    }
+  },
+  created () {
+    setTimeout(this.changej, 5000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    color: #42b983;
+  }
+  .follo1 {
+    font-style: italic;
+    font-weight: lighter;
+  }
+  .follo2 {
+    color: red;
+  }
+  .follo3 {
+    color: blue;
+  }
+</style>
